4ae04274bb08ca1dfe452f15827e6434826548c2
Neocoda <neocoda.work@gmail.com>
2023-02-03 23:48:35 +0300

    add cli.py

---
+++ brain_games/scripts/cli.py
@@ -0,0 +1,7 @@
+import prompt
+
+
+def welcome_user():
+    name = prompt.string('Greetings, mysterious stranger, what is your name?: ')
+    print(f'\nWelcome to the Brain Games {name}! Mu-ha-ha!\
+            \nThere is no way out of here. Literally.')
